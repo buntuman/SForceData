9671520e99ba7a05c7d996a4e558f776bc77998b
buntuman <buntuman@LAPTOP-S10HEVBP.localdomain>
2019-03-02 13:30:52 -0500

    added functionality to access a collection of both warehousedandshipped kits

--- forceData.py
+++ forceData.py
@@ -1,3 +1,17 @@
+'''
+@Author : Daniel Gitahi
+
+*This module requires a working internet connection and valid salesforce credentials for the company.
+Please contact the IT administrator to acquire the above mentioned requirements if you do not already
+have them.
+
+*This Document contains the definition of a ForceConnection class that allows the user to
+easily extract data from companies salesforce instance. This module has been customized to
+make it easier for shipped and warehoused parts to be retrieved.
+    NB.The kits, as stored in salesforce, are identifers that will help the user retrieve individual
+    parts. Therefore, a kit is a collection of individual parts.
+'''
+
 from simple_salesforce import Salesforce
 from urllib.request import urlopen
 import numpy as np
@@ -33,16 +47,19 @@ class ForceConnection:
             for record in records:
                 self.shippedKits.append(record['CKSW_BASE__Serial_Number__c'])
             return self.shippedKits
-
+    #The 'getWarehousedKits' method returns a list of kits identifiers when successfull. Otherwise, it returns False
     def getWarehousedKits(self):
         if(self.forceConnect()):
             warehouseQry = "SELECT CKSW_BASE__Serial_Number__c FROM CKSW_BASE__Product_Instance__c WHERE Model_Number__c LIKE '%board%' AND Location_Ref__c LIKE '%WAREHOUSE%'"
-            result = self.conn.query_all(warehouseQry)
-            records = result['records']
-            for record in records:
-                self.warehousedKits.append(record['CKSW_BASE__Serial_Number__c'])
-            return self.warehousedKits
-
+            try:
+                result = self.conn.query_all(warehouseQry)
+                records = result['records']
+                for record in records:
+                    self.warehousedKits.append(record['CKSW_BASE__Serial_Number__c'])
+                return self.warehousedKits
+            except:
+                self.err.append("Invalid Query")
+                return False
     def networkConnection(self):
         try:
             urlopen("http://www.google.com")
