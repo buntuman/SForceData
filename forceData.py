006c74d78b07a9032cba0d135126488cc2bd051a
buntuman <buntuman@LAPTOP-S10HEVBP.localdomain>
2019-03-02 10:21:07 -0500

    added shipped and warehoused units retrieval functionality

--- forceData.py
+++ forceData.py
@@ -1,5 +1,6 @@
 from simple_salesforce import Salesforce
 from urllib.request import urlopen
+import numpy as np
 
 class ForceConnection:
     def __init__(self,username=None,password=None,securityToken=None):
@@ -9,6 +10,10 @@ class ForceConnection:
         self.securtiyToken = securityToken
         self.err=list()
         self.conn=None
+        self.warehousedKits = list()
+        self.shippedKits = list()
+    def getWarehousedAndShippedKits(self):
+        return self.getShippedKits()+self.getWarehousedKits()
     def getError(self):
         if(len(self.err) == 0 ):
             self.err.append("No errors")
@@ -21,15 +26,22 @@ class ForceConnection:
                 (len(self.password.strip()) != 0 ) and
                 (len(self.securtiyToken) != 0))
     def getShippedKits(self):
-        if(self.forceConnect()):
-            shippedQry=""
-
-        pass
+        if (self.forceConnect()):
+            shippedQry = "SELECT CKSW_BASE__Serial_Number__c FROM CKSW_BASE__Product_Instance__c WHERE Model_Number__c LIKE '%board%' AND Location_Ref__c LIKE '%shipped%'"
+            result = self.conn.query_all(shippedQry)
+            records = result['records']
+            for record in records:
+                self.shippedKits.append(record['CKSW_BASE__Serial_Number__c'])
+            return self.shippedKits
 
     def getWarehousedKits(self):
         if(self.forceConnect()):
             warehouseQry = "SELECT CKSW_BASE__Serial_Number__c FROM CKSW_BASE__Product_Instance__c WHERE Model_Number__c LIKE '%board%' AND Location_Ref__c LIKE '%WAREHOUSE%'"
-
+            result = self.conn.query_all(warehouseQry)
+            records = result['records']
+            for record in records:
+                self.warehousedKits.append(record['CKSW_BASE__Serial_Number__c'])
+            return self.warehousedKits
 
     def networkConnection(self):
         try:
