2ff796ca0dcfefe00bff71a5da4c1a48bed73af5
buntuman <buntuman@LAPTOP-S10HEVBP.localdomain>
2019-03-04 19:03:25 -0500

    Initial commit. Started defining the 'component' class

---
+++ component.py
@@ -0,0 +1,11 @@
+'''
+@Author :Daniel Gitahi
+This document contains the definition of a 'component' object.
+'''
+
+class Component:
+    def __init__(self):
+        self.serial = None
+        self.model = None
+
+    def
